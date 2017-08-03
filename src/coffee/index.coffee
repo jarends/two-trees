6b72da3a672610da7e64c8cb7db777bd739d7b31
Jan-Olaf Arends <jan.arends@definitely.eu>
2017-08-03 22:45:29 +0200

    view-tree officially exported

--- src/coffee/index.coffee
+++ src/coffee/index.coffee
@@ -1,4 +1,5 @@
 module.exports =
     DataTree: require './data-tree'
+    ViewTree: require './view-tree'
     ViewNode: require './view-node'
     CompNode: require './comp-node'
--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@
 (function() {
   module.exports = {
     DataTree: require('./data-tree'),
+    ViewTree: require('./view-tree'),
     ViewNode: require('./view-node'),
     CompNode: require('./comp-node')
   };
