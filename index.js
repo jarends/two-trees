e6b0025a8b97d736dc75ecc038a027c98c27b40a
Jan-Olaf Arends <jan.arends@definitely.eu>
2017-08-17 21:38:33 +0200

    utils exported

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 (function()
 {
     module.exports = {
-        DataTree:require('./src/js/data-tree'),
-        ViewTree:require('./src/js/view-tree'),
-        ViewNode:require('./src/js/view-node'),
-        CompNode:require('./src/js/comp-node'),
-        ViewTreeSmall:require('./src/js/view-tree-small'),
-        ViewNodeSmall:require('./src/js/view-node-small'),
+        DataTree:      require('./src/js/data-tree'),
+        ViewTree:      require('./src/js/view-tree'),
+        ViewNode:      require('./src/js/view-node'),
+        CompNode:      require('./src/js/comp-node'),
+        ViewTreeSmall: require('./src/js/view-tree-small'),
+        ViewNodeSmall: require('./src/js/view-node-small'),
+        utils:         require('./src/js/utils')
     };
 
 }).call(this);
